608a26dbf28301992f43822be292d3c9dd7ae83b
SC Graphics <scgraphics@SCs-iMac.local>
2018-06-14 13:41:24 -0400

    Init repo

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="blog in blogs">
+      {{ blog.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+    apollo: {
+      blogs: gql`{
+        blogs {
+          title
+          body
+        }
+      }`,
+    }
+}
+</script>
